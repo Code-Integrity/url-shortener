5ac781e6067f48f40e3b54455f794a5514a28a0c
Code-Integrity <wyolw239@gmail.com>
2026-09-15 12:57:15 +0900

    fix: manually initialize migration lock provider as postgresql

--- backend/prisma/migrations/20260914043703_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Url" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "originalUrl" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
--- CreateIndex
-CREATE INDEX "Url_id_idx" ON "Url"("id");
